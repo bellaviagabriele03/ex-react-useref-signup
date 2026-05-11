eda1da2b302fb1dab25e0b35dcf4e97a391695f3
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-11 13:16:15 +0200

    FEAT: page HOME done, (design by-Loris)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,15 @@ export default function Home() {
     return (
         <div className="container">
             <h1 className="title">HOME</h1>
+            <p className="home-content">
+                ESERCITAZIONE DI  "useRef" <br />
+                REPO: ex-react-useref-signup
+                <div className="list-milestone">
+                    <span>📌 Milestone 1: Creare un Form con Campi Controllati</span>
+                    <span>📌 Milestone 2: Validare in tempo reale</span>
+                    <span>📌 Milestone 3: Convertire i Campi Non Controllati</span>
+                </div>
+            </p>
         </div>
 
     )
